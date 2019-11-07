26b5c7848d5fd9e81b5b2ca064060d82091c90f4
Bean <ihubnetceogeenomc@gmail.com>
2019-11-06 16:59:00 -0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use(express.static("src"));
 
 
 app.get("/", function(request, response) {
-  response.sendFile(__dirname + "/src/index.html");
+  response.sendFile(__dirname + "/html/index.html");
 });
 
 app.get("/route/discord", function(request, response) {
