6743561cd2c88746ba744163524bee2bd6928e4b
Bean <ihubnetceogeenomc@gmail.com>
2019-11-07 16:49:33 -0800

    Update force-uninstaller.bat

--- src/force-uninstaller.bat
+++ src/force-uninstaller.bat
@@ -3,12 +3,5 @@ color a
 title Uninstaller
 cd %~dp0
 cls
-choice /C 12 /M "Are you sure you want to uninstall ExpressRouter? [1 = Yes; 2 = No]"
-
-:: E
-IF ERRORLEVEL 2 exit
-IF ERRORLEVEL 1 GOTO Dropper
-
-:Dropper
 move %~dp0\exec-uninstall.bat %~dp0\..\..
 start %~dp0\..\..\exec-uninstall.bat
